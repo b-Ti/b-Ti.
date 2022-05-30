8f4348eab12c6827c4d4ee5831381ef25c131023
b-Ti <2585365256@qq.com>
2022-05-30 11:32:30 +0800

    新增

---
+++ first_flutter_app/flutter_application/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\tools\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\BAITINGITNG\\Desktop\\创新实验\\first_flutter_app\\flutter_application" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\tools\\flutter"
+  "PROJECT_DIR=C:\\Users\\BAITINGITNG\\Desktop\\创新实验\\first_flutter_app\\flutter_application"
+  "FLUTTER_ROOT=C:\\tools\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\BAITINGITNG\\Desktop\\创新实验\\first_flutter_app\\flutter_application\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\BAITINGITNG\\Desktop\\创新实验\\first_flutter_app\\flutter_application"
+  "FLUTTER_TARGET=C:\\Users\\BAITINGITNG\\Desktop\\创新实验\\first_flutter_app\\flutter_application\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\BAITINGITNG\\Desktop\\创新实验\\first_flutter_app\\flutter_application\\.dart_tool\\package_config.json"
+)
